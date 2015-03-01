460955ac783bc35d352dfc4001953d0563004989
simontaylor81 <simontaylor81@hotmail.com>
2015-03-01 02:07:30 +0000

    Factored texture test into common util function

--- SRPTests/TestScripts/Utils/utils.py
+++ SRPTests/TestScripts/Utils/utils.py
@@ -1,2 +1,13 @@
-def myprint(s):
-	print s
+# Texture testing helper.
+def TestTexture(tex, ri):
+	# Compile shaders.
+	vs = ri.LoadShader("FullscreenTexture.hlsl", "FullscreenTexture_VS", "vs_4_0")
+	ps = ri.LoadShader("FullscreenTexture.hlsl", "FullscreenTexture_PS", "ps_4_0")
+	
+	ri.SetShaderResourceVariable(ps, "tex", tex)
+	
+	def RenderFrame(context):
+		# Clear the lighting buffer (i.e. the back buffer).
+		context.DrawFullscreenQuad(vs, ps)
+	
+	ri.SetFrameCallback(RenderFrame)
