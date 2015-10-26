3166351b7904be29f40f71d28c54f08ff31dfd97
Simon Taylor <simontaylor81@hotmail.com>
2015-10-26 10:24:35 +0000

    PBR sample: switch between IBL and no IBL

--- Samples/PBR/pbr.py
+++ Samples/PBR/pbr.py
@@ -1,23 +1,27 @@
 from SRPScripting import *
 
-defines = { 'PBR_USE_IBL': 1 }
+vs = ri.CompileShader("pbr.hlsl", "BasicVS", "vs_4_0")
 
-vs = ri.CompileShader("pbr.hlsl", "BasicVS", "vs_4_0", defines)
-ps = ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", defines)
+ps_ibl = ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", { 'PBR_USE_IBL': 1 })
+ps_noibl = ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", { 'PBR_USE_IBL': 0 })
 
 backgroundVs = ri.CompileShader("background.hlsl", "VS", "vs_4_0")
 backgroundPs = ri.CompileShader("background.hlsl", "PS", "ps_4_0")
 
-ri.BindShaderVariableToMaterial(ps, "SolidColour", "DiffuseColour")
+ri.BindShaderVariableToMaterial(ps_ibl, "SolidColour", "DiffuseColour")
+ri.BindShaderVariableToMaterial(ps_noibl, "SolidColour", "DiffuseColour")
+
+useIbl = ri.AddUserVar("Use IBL", UserVariableType.Bool, True)
 
 # Load environment cubemap.
 envmap = ri.LoadTexture("assets/Arches_E_PineTree_Cube.dds")
 ri.SetShaderResourceVariable(backgroundPs, "EnvCube", envmap)
-ri.SetShaderResourceVariable(ps, "EnvCube", envmap)
+ri.SetShaderResourceVariable(ps_ibl, "EnvCube", envmap)
+ri.SetShaderResourceVariable(ps_noibl, "EnvCube", envmap)
 
 def RenderFrame(context):
 	context.Clear((0.5, 0.5, 1.0, 0))
 	context.DrawFullscreenQuad(backgroundVs, backgroundPs)
-	context.DrawScene(vs, ps)
+	context.DrawScene(vs, ps_ibl if useIbl() else ps_noibl)
 
 ri.SetFrameCallback(RenderFrame)
