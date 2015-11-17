8f0f0f4508c28095a6197a6056e3b71ef5295ba8
simontaylor81 <simontaylor81@hotmail.com>
2015-11-17 14:40:07 +0000

    Added long running script to test project
    For testing async script execution stuff

---
+++ testproject/LongRunningScript.py
@@ -0,0 +1,15 @@
+from SRPScripting import *
+import time
+
+vs = ri.CompileShader("BasicShaders.hlsl", "BasicVS", "vs_4_0")
+ps = ri.CompileShader("BasicShaders.hlsl", "SolidColourPS", "ps_4_0")
+
+ri.BindShaderVariableToMaterial(ps, "SolidColour", "DiffuseColour")
+
+# Insert long delay to test async script execution.
+time.sleep(5)
+
+def RenderFrame(context):
+	context.DrawScene(vs, ps)
+
+ri.SetFrameCallback(RenderFrame)
