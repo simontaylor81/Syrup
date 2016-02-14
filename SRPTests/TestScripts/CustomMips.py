42c4da3af3e81b59e9acd310a5ecff0878fb3bbf
Simon Taylor <simontaylor81@hotmail.com>
2016-02-14 13:18:54 +0100

    Autotests for custom mip generation

--- SRPTests/TestScripts/CustomMips.py
+++ SRPTests/TestScripts/CustomMips.py
@@ -1,5 +1,8 @@
 # Test custom mipmap generation.
 import utils
 
-tex = ri.LoadTexture('Assets/Textures/ThisIsATest.png', generateMips = 'CustomDownsample.hlsl')
-utils.TestTextureLevel(ri, tex, 1)
+utils.TestSetting('sampler', 'linear', globals())
+utils.TestSetting('mip', 1, globals())
+filename = 'CustomDownsample_2D_' + sampler + '.hlsl'
+tex = ri.LoadTexture('Assets/Textures/ThisIsATest.png', generateMips = filename)
+utils.TestTextureLevel(ri, tex, mip)
--- SRPTests/TestScripts/Shaders/CustomDownsample.hlsl
+++
@@ -1,5 +0,0 @@
-
-float4 Sample(float2 uv)
-{
-	return Texture.SampleLevel(LinearSampler, uv, DestMip - 1);
-}
