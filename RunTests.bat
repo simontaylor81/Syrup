4df40b98938e1684729841c7c2197e52ba654228
Simon Taylor <simontaylor81@hotmail.com>
2016-03-20 19:39:32 +0100

    Fixed RunTests.bat using wrong config

--- RunTests.bat
+++ RunTests.bat
@@ -5,7 +5,7 @@ SETLOCAL
 CALL "%VS140COMNTOOLS%VsMSBuildCmd.bat"
 
 ECHO Building solution...
-msbuild .\ShaderEditor.sln /property:Configuration=Debug /property:Platform=x86 /verbosity:minimal /nologo
+msbuild .\ShaderEditor.sln /property:Configuration=Debug /verbosity:minimal /nologo
 
 ECHO Running tests...
-.\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe .\SRPTests\bin\x86\Debug\SRPTests.dll
+.\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe .\SRPTests\bin\Debug\SRPTests.dll
