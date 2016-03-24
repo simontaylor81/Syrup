eef4b77b1f0c394fda4b3ccb2ce3d99e1bcf3bad
Simon Taylor <simontaylor81@hotmail.com>
2016-03-24 19:59:50 +0100

    String categories for output window
    Still a total mess

--- ShaderEditorApp/OutputWindow.xaml.cs
+++ ShaderEditorApp/OutputWindow.xaml.cs
@@ -20,36 +20,22 @@ namespace ShaderEditorApp
 	/// <summary>
 	/// Interaction logic for OutputWindow.xaml
 	/// </summary>
+	/// TODO: MVVM- & Rx-ify
 	public partial class OutputWindow : UserControl, ILoggerFactory
 	{
 		public OutputWindow()
 		{
 			InitializeComponent();
-
-			// Create per-category text boxes and fill category combo box items.
-			foreach (LogCategory category in Enum.GetValues(typeof(LogCategory)))
-			{
-				// Add to combo box.
-				categoryCombo.Items.Add(category.ToString());
-
-				// Create text box. Properties automatically come from style in XAML.
-				var textBox = new TextBox();
-
-				// Add to control and list.
-				grid.Children.Add(textBox);
-				categoryTextBoxes[(int)category] = textBox;
-			}
-			CurrentCategory = LogCategory.Script;
 		}
 
 		// Add a message to the output window.
-		private void Log(LogCategory category, string text)
+		private void Log(string category, string text)
 		{
 			// This function can be called on any thread, but we need to access the UI on the UI thread,
 			// so post back to the WPF dispatcher.
 			Application.Current.Dispatcher.InvokeAsync(() =>
 				{
-					var outputText = categoryTextBoxes[(int)category];
+					var outputText = GetOrCreateCategoryTextBox(category);
 					bool bAutoScroll = outputText.CaretIndex == outputText.Text.Length;
 
 					outputText.AppendText(text);
@@ -60,78 +46,99 @@ namespace ShaderEditorApp
 						outputText.ScrollToEnd();
 						outputText.CaretIndex = outputText.Text.Length;
 					}
+
+					// Make the logged-to text box active so you can see the output.
+					CurrentCategory = category;
 				});
 		}
 
 		// Clear the output for a category.
-		private void Clear(LogCategory category)
+		private void Clear(string category)
 		{
 			Application.Current.Dispatcher.InvokeAsync(() =>
 				{
-					categoryTextBoxes[(int)category].Clear();
+					// Don't create a text box just to clear it.
+					categoryTextBoxes.GetOrDefault(category)?.Clear();
 				});
 		}
 
 		private void clearButton_Click(object sender, RoutedEventArgs e)
 		{
 			// Clear the current category's text box.
-			CurrentTextbox.Clear();
+			CurrentTextbox?.Clear();
+		}
+
+		// Get the text box for a category. Adds one if it doesn't exist.
+		private TextBox GetOrCreateCategoryTextBox(string category)
+		{
+			return categoryTextBoxes.GetOrAdd(category, () =>
+			{
+				// Add to combo box.
+				categoryCombo.Items.Add(category);
+
+				// Create text box. Properties automatically come from style in XAML.
+				var textBox = new TextBox();
+
+				// Add to control.
+				grid.Children.Add(textBox);
+
+				return textBox;
+			});
 		}
 
 		// Array of text box controls, one for each category.
 		// We have a separate text box for each one so it each maintains its own state, like scroll position.
-		private TextBox[] categoryTextBoxes = new TextBox[Enum.GetNames(typeof(LogCategory)).Length];
+		private Dictionary<string, TextBox> categoryTextBoxes = new Dictionary<string, TextBox>();
 
 		// Get/set the currently displayed category.
-		private LogCategory CurrentCategory
+		private string CurrentCategory
 		{
 			// Just mirror the selection index of the combo box.
 			// Setting triggers SelectionChanged event, as desired.
-			get { return (LogCategory)categoryCombo.SelectedIndex; }
-			set { categoryCombo.SelectedIndex = (int)value; }
+			get { return (string)categoryCombo.SelectedValue; }
+			set
+			{
+				if (CurrentCategory != value)
+				{
+					categoryCombo.SelectedValue = value;
+
+					// HACK: update UI.
+					categoryCombo_SelectionChanged(null, null);
+				}
+			}
 		}
 
 		// Get the text box corresponding to the current category.
-		private TextBox CurrentTextbox => categoryTextBoxes[(int)CurrentCategory];
+		private TextBox CurrentTextbox => CurrentCategory != null ? categoryTextBoxes.GetOrDefault(CurrentCategory) : null;
 
 		// Called when the category selection is changed, either in the UI or programmatically.
 		private void categoryCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			// Hide all text boxes;
-			foreach (var textBox in categoryTextBoxes)
-				textBox.Visibility = Visibility.Collapsed;
+			foreach (var kvp in categoryTextBoxes)
+			{
+				kvp.Value.Visibility = Visibility.Collapsed;
+			}
 
 			// Show the current one.
-			CurrentTextbox.Visibility = Visibility.Visible;
+			if (CurrentTextbox != null)
+			{
+				CurrentTextbox.Visibility = Visibility.Visible;
+			}
 		}
 
 		public ILogger CreateLogger(string category)
 		{
-			// TODO: Replace LogCategory with strings.
-			LogCategory categoryEnum;
-			if (Enum.TryParse(category, true, out categoryEnum))
-			{
-				return new OutputWindowLogger(this, categoryEnum);
-			}
-			return new NullLogger();
-		}
-
-		// TODO: REMOVE
-		private enum LogCategory
-		{
-			Log,            // General debug log message
-			Script,         // Script output
-			ShaderCompile,  // Shader compilation output
+			return new OutputWindowLogger(this, category);
 		}
 
 		// Logger that writes to the output window.
 		private class OutputWindowLogger : ILogger
 		{
-			private readonly LogCategory _category;
+			private readonly string _category;
 			private readonly OutputWindow _outputWindow;
 
-			// TODO: Replace LogCategory with strings.
-			public OutputWindowLogger(OutputWindow outputWindow, LogCategory category)
+			public OutputWindowLogger(OutputWindow outputWindow, string category)
 			{
 				_outputWindow = outputWindow;
 				_category = category;
