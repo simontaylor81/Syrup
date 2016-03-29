6afa82095af31104b13afa36461483849eaa03fe
simontaylor81 <simontaylor81@hotmail.com>
2016-03-29 20:23:06 +0200

    Refactored pbr sample to use script rerun
    Read "Use IBL" user var in main script execution rather than frame
    callback. Use it to only compile the shader we need, rather than both.
    
    Means you can iterate on the (fast-compiling) no-IBL version without
    having to recompile the (very-slow-compiling) IBL version.
    
    Also simplifies the script a bit.

--- Samples/PBR/pbr.py
+++ Samples/PBR/pbr.py
@@ -1,13 +1,10 @@
 from SRPScripting import *
 
+useIblVar = ri.AddUserVar_Bool("Use IBL", True)
+useIbl = 1 if useIblVar() else 0
+
 vs = ri.CompileShader("pbr.hlsl", "BasicVS", "vs_4_0")
-
-ps_ibl = ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", { 'PBR_USE_IBL': 1 })
-ps_noibl = ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", { 'PBR_USE_IBL': 0 })
-
-# TEMP: Compile loads of shaders to test parallel compilation.
-#for i in xrange(0, 50):
-#	ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", { 'PBR_USE_IBL': 1, 'DUMMY': i })
+ps = ri.CompileShader("pbr.hlsl", "SolidColourPS", "ps_4_0", { 'PBR_USE_IBL': useIbl })
 
 backgroundVs = ri.CompileShader("background.hlsl", "VS", "vs_4_0")
 backgroundPs = ri.CompileShader("background.hlsl", "PS", "ps_4_0")
@@ -15,27 +12,20 @@ backgroundPs = ri.CompileShader("background.hlsl", "PS", "ps_4_0")
 # Load environment cubemap.
 envmap = ri.LoadTexture("assets/Arches_E_PineTree_Cube.dds")
 
-def BindPixelShaderVars(ps):
-	ps.FindConstantVariable("BaseColour").BindToMaterial("BaseColour")
-	ps.FindConstantVariable("PbrParams").BindToMaterial("PbrParams")
+ps.FindConstantVariable("BaseColour").BindToMaterial("BaseColour")
+ps.FindConstantVariable("PbrParams").BindToMaterial("PbrParams")
 
-	ps.FindResourceVariable("BaseColourTex").BindToMaterial("BaseColour", fallback = ri.WhiteTexture);
-	ps.FindResourceVariable("SmoothnessTex").BindToMaterial("Smoothness", fallback = ri.WhiteTexture);
-	ps.FindResourceVariable("MetallicTex").BindToMaterial("Metallic", fallback = ri.WhiteTexture);
-	ps.FindResourceVariable("NormalTex").BindToMaterial("Normal", fallback = ri.DefaultNormalTexture);
-	
-	ps.FindResourceVariable("EnvCube").Set(envmap);
+ps.FindResourceVariable("BaseColourTex").BindToMaterial("BaseColour", fallback = ri.WhiteTexture);
+ps.FindResourceVariable("SmoothnessTex").BindToMaterial("Smoothness", fallback = ri.WhiteTexture);
+ps.FindResourceVariable("MetallicTex").BindToMaterial("Metallic", fallback = ri.WhiteTexture);
+ps.FindResourceVariable("NormalTex").BindToMaterial("Normal", fallback = ri.DefaultNormalTexture);
 
-	
-BindPixelShaderVars(ps_ibl)
-BindPixelShaderVars(ps_noibl)
+ps.FindResourceVariable("EnvCube").Set(envmap);
 backgroundPs.FindResourceVariable("EnvCube").Set(envmap);
-
-useIbl = ri.AddUserVar_Bool("Use IBL", True)
 
 def RenderFrame(context):
 	context.Clear((0.5, 0.5, 1.0, 0))
 	context.DrawFullscreenQuad(backgroundVs, backgroundPs)
-	context.DrawScene(vs, ps_ibl if useIbl() else ps_noibl)
+	context.DrawScene(vs, ps)
 
 ri.SetFrameCallback(RenderFrame)
