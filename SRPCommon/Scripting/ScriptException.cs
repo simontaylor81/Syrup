041b2db35bef17ff22ee50e52462f02edd14cd94
simontaylor81 <simontaylor81@hotmail.com>
2016-04-02 23:17:10 +0200

    Put ScriptException in its own class

---
+++ SRPCommon/Scripting/ScriptException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPCommon.Scripting
+{
+	// Special exception class to throw when the user does something wrong.
+	// Nothing special about it, but it allows us to ignore it explicitly in the debugger.
+	// You should add "ShaderEditorApp.ScriptException" to the Debug->Exceptions window and disable break when user-unhandled.
+	public class ScriptException : Exception
+	{
+		public ScriptException()
+			: base()
+		{ }
+		public ScriptException(string message)
+			: base(message)
+		{ }
+		public ScriptException(string message, Exception innerException)
+			: base(message, innerException)
+		{ }
+	}
+}
--- SRPCommon/Scripting/Scripting.cs
+++ SRPCommon/Scripting/Scripting.cs
@@ -157,20 +157,4 @@ namespace SRPCommon.Scripting
 			}
 		}
 	}
-
-	// Special exception class to throw when the user does something wrong.
-	// Nothing special about it, but it allows us to ignore it explicitly in the debugger.
-	// You should add "ShaderEditorApp.ScriptException" to the Debug->Exceptions window and disable break when user-unhandled.
-	public class ScriptException : Exception
-	{
-		public ScriptException()
-			: base()
-		{}
-		public ScriptException(string message)
-			: base(message)
-		{}
-		public ScriptException(string message, Exception innerException)
-			: base(message, innerException)
-		{}
-	}
 }
