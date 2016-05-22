f511484a94f7f3533471051a85671bb1d127d398
Simon Taylor <simontaylor81@hotmail.com>
2016-05-22 17:01:01 +0200

    First pass Non-ListView-based properties window
    Use a grid and StackPanel combo to allow more flexibility in the
    controls we use for the property controls. Plus removes the rather
    pointless header row.

--- ShaderEditorApp/SampleData/DummyViewModels.cs
+++ ShaderEditorApp/SampleData/DummyViewModels.cs
@@ -1,5 +1,4 @@
-﻿using SRPCommon.UserProperties;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +8,51 @@ namespace ShaderEditorApp.SampleData
 {
 	public class DummyWorkspace
 	{
-		public List<DummyPropertyBase> Properties { get; set; }
+		public List<DummyPropertyBase> Properties { get; set; } = new List<DummyPropertyBase>();
+
+		public DummyWorkspace()
+		{
+			Properties.Add(new DummyPropertyFloat { DisplayName = "Property 1", IsReadOnly = false, Value = 12.0f });
+			Properties.Add(new DummyPropertyFloat { DisplayName = "Property 2", IsReadOnly = true, Value = -7.0f });
+			Properties.Add(new DummyPropertyBool { DisplayName = "Bool prop", IsReadOnly = false, Value = true });
+			Properties.Add(new DummyChoiceProperty
+			{
+				DisplayName = "Choice prop",
+				IsReadOnly = false,
+				Value = "Choice 1",
+				Choices = { "Hello", "World", }
+			});
+			Properties.Add(new DummyVectorProperty
+			{
+				DisplayName = "Vec prop",
+				IsReadOnly = true,
+				SubProperties =
+				{
+					new DummyPropertyFloat { DisplayName="X", IsReadOnly=false, Value=1.00001f },
+					new DummyPropertyFloat { DisplayName="Y", IsReadOnly=false, Value=2.0f },
+					new DummyPropertyFloat { DisplayName="Z", IsReadOnly=false, Value=3.0f },
+					new DummyPropertyFloat { DisplayName="W", IsReadOnly=false, Value=4.0f },
+				}
+			});
+			Properties.Add(new DummyMatrixProperty
+			{
+				DisplayName = "Mat prop",
+				IsReadOnly = true,
+				Rows =
+				{
+					new DummyPropertyList
+					{
+						new DummyPropertyFloat { DisplayName="X", IsReadOnly=false, Value=1.00001f },
+						new DummyPropertyFloat { DisplayName="Y", IsReadOnly=false, Value=2.0f },
+					},
+					new DummyPropertyList
+					{
+						new DummyPropertyFloat { DisplayName="Z", IsReadOnly=false, Value=3.0f },
+						new DummyPropertyFloat { DisplayName="W", IsReadOnly=false, Value=4.0f },
+					}
+				}
+			});
+		}
 	}
 
 	public class DummyPropertyBase
@@ -28,16 +71,16 @@ namespace ShaderEditorApp.SampleData
 	}
 	public class DummyVectorProperty : DummyPropertyBase
 	{
-		public List<DummyPropertyBase> SubProperties { get; set; }
+		public List<DummyPropertyBase> SubProperties { get; set; } = new List<DummyPropertyBase>();
 	}
 	public class DummyMatrixProperty : DummyPropertyBase
 	{
-		public List<DummyPropertyList> Rows { get; set; }
+		public List<DummyPropertyList> Rows { get; set; } = new List<DummyPropertyList>();
 	}
 	public class DummyChoiceProperty : DummyPropertyBase
 	{
 		public string Value { get; set; }
-		public List<string> Choices { get; set; }
+		public List<string> Choices { get; set; } = new List<string>();
 	}
 
 	public class DummyPropertyList : List<DummyPropertyBase>
