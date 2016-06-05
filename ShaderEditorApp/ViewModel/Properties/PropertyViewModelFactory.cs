491ca0c40db6ca9e3c2cc601c67c39b8350e6d35
simontaylor81 <simontaylor81@hotmail.com>
2016-06-05 23:14:46 +0200

    Minor cleanup

--- ShaderEditorApp/ViewModel/Properties/PropertyViewModelFactory.cs
+++ ShaderEditorApp/ViewModel/Properties/PropertyViewModelFactory.cs
@@ -1,28 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SRPCommon.UserProperties;
 
 namespace ShaderEditorApp.ViewModel.Properties
 {
 	static class PropertyViewModelFactory
 	{
-		private static bool _initialised = false;
-		private static IEnumerable<IPropertyViewModelFactory> _factories;
+		private static Lazy<IEnumerable<IPropertyViewModelFactory>> _factories = new Lazy<IEnumerable<IPropertyViewModelFactory>>(FindFactories);
 
 		// Create a new view-model object for a user property.
 		public static PropertyViewModel CreateViewModel(IUserProperty property)
 		{
-			if (!_initialised)
-			{
-				FindFactories();
-				_initialised = true;
-			}
-
 			// Find the first factory that accepts the property.
-			var factory = _factories.FirstOrDefault(x => x.SupportsProperty(property));
+			var factory = _factories.Value.FirstOrDefault(x => x.SupportsProperty(property));
 			if (factory != null)
 			{
 				return factory.CreateInstance(property);
@@ -31,10 +22,10 @@ namespace ShaderEditorApp.ViewModel.Properties
 			throw new ArgumentException("Unsupported property type");
 		}
 
-		private static void FindFactories()
+		private static IEnumerable<IPropertyViewModelFactory> FindFactories()
 		{
-			// Find all types that implement the factory interface.
-			_factories = typeof(PropertyViewModelFactory).Assembly.GetTypes()
+			// Find all types that implement the factory interface in this assembly.
+			return typeof(PropertyViewModelFactory).Assembly.GetTypes()
 				.Where(type => !type.IsAbstract && type.GetInterfaces().Any(i => i == typeof(IPropertyViewModelFactory)))
 				.Select(type => (IPropertyViewModelFactory)Activator.CreateInstance(type))
 				.OrderBy(factory => factory.Priority)
